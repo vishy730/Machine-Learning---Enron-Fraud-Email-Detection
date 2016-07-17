6d0c3e55b68e2f4238e55b2a3e1d08d1a48b1289
vishy730 <vishy730@gmail.com>
2016-07-17 20:56:51 +0530

    Add files via upload

---
+++ poi_id.py
@@ -0,0 +1,172 @@
+import sys
+import pickle
+
+sys.path.append("../tools/")
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import os
+
+
+from feature_format import featureFormat, targetFeatureSplit
+from tester import test_classifier, dump_classifier_and_data
+
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+features_list = ['poi',
+                 'salary',
+                 'fraction_from_poi',
+                 'exercised_stock_options',
+                 'shared_receipt_with_poi',
+                 'expenses']
+
+### Load the dictionary containing the dataset
+data_dict = pickle.load(open("final_project_dataset.pkl", "r"))
+
+### Task 2: Remove outliers
+data_dict.pop('TOTAL', 0)
+data_dict.pop('THE TRAVEL AGENCY IN THE PARK', 0)
+### Task 3: Create new feature(s)
+###Create 3 new features
+# get the top 3 bonuses and top 3 salaries and put into set
+from sets import Set
+
+specific_emails = Set()
+tmp = []
+for key in data_dict.keys():
+    if data_dict[key]["salary"] != 'NaN' and data_dict[key]["bonus"] != 'NaN' and data_dict[key]["poi"] == True:
+        tmp.append({"key": key, "salary": data_dict[key]["salary"], "bonus": data_dict[key]["bonus"],
+                    "email_address": data_dict[key]["email_address"]})
+
+
+###Getting emails of n top of persons sortered by feature_name feature
+###Receiving Set of emails something like (['tim.belden@enron.com', 'kenneth.lay@enron.com', 'jeff.skilling@enron.com'])
+def add_to_specific_email_set(feature_name, n):
+    cleaned_data = sorted(tmp, key=lambda (x): x[feature_name], reverse=True)
+    cleaned_data = cleaned_data[0:n]
+    for email in cleaned_data:
+        specific_emails.add(email["email_address"])
+
+
+add_to_specific_email_set("salary", 5)
+add_to_specific_email_set("bonus", 5)
+
+###getting all to_emails from current message
+to_emails_dict = {}
+
+def getToEmails(fname):
+    with open(fname) as f:
+        content = f.readlines()
+        for c in content:
+            if 'To:' in c:
+                to_emails = c.replace(" ", "").split(':')[1][:-2].split(',')
+                break
+    return to_emails
+
+
+###counting all messages from specific emails
+for email in specific_emails:
+    try:
+        list_messages = open(os.getcwd() + "/emails_by_address/" + "from_" + email + ".txt", "r")
+        for path_str in list_messages:
+            path = os.path.join('..', path_str[:-1])
+            to_addresses = getToEmails(path)
+            for addr in to_addresses:
+                if addr not in to_emails_dict.keys():
+                    to_emails_dict[addr] = {"from_specific_email": 1}
+                else:
+                    to_emails_dict[addr]["from_specific_email"] += 1
+    except:
+        pass
+
+
+###compute fraction
+def computeFraction(poi_messages, all_messages):
+    """ given a number messages to/from POI (numerator)
+        and number of all messages to/from a person (denominator),
+        return the fraction of messages to/from that person
+        that are from/to a POI
+   """
+    if poi_messages != 'NaN' and all_messages != 'NaN' and all_messages != 0:
+        fraction = float(poi_messages) / float(all_messages)
+    else:
+        fraction = 0.
+    return fraction
+
+
+###creating 3 new features fraction_from_poi, fraction_to_poi (like in example) and from_specific_email
+for key in data_dict:
+    data_point = data_dict[key]
+    email_key = data_point["email_address"]
+    data_point["fraction_from_poi"] = computeFraction(data_point["from_this_person_to_poi"],
+                                                      data_point["from_messages"])
+    data_point["fraction_to_poi"] = computeFraction(data_point["from_poi_to_this_person"], data_point["to_messages"])
+    if email_key in to_emails_dict.keys():
+        data_point["from_specific_email"] = to_emails_dict[email_key]["from_specific_email"]
+    else:
+        data_point["from_specific_email"] = 0
+
+### Store to new_dataset for easy export below.
+new_dataset = data_dict
+
+### Extract features and labels from dataset for local testing
+
+data = featureFormat(new_dataset, features_list, sort_keys=True)
+labels, features = targetFeatureSplit(data)
+
+
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
+
+#Naive Bayes Classification
+from sklearn.naive_bayes import GaussianNB
+# clf = GaussianNB()
+# clf.fit(features, labels)
+
+#Support Vector Classification
+from sklearn.svm import SVC
+# clf = SVC(C=10000, kernel="rbf")
+# clf.fit(features, labels)
+
+#Decision Tree Classification
+from sklearn import tree
+#
+clf = tree.DecisionTreeClassifier(criterion='gini',
+                                  max_depth=6, max_features=None, max_leaf_nodes=None,
+                                  min_samples_leaf=1, min_samples_split=12,
+                                  random_state=42, splitter='best')
+clf = clf.fit(features, labels)
+
+
+
+###feature importances for feature selection process
+feature_importances = clf.feature_importances_
+sorted_idx = (-np.array(feature_importances)).argsort()
+print "Rank of features"
+for idx in sorted_idx:
+    print "{:4f} : {}".format(feature_importances[idx], features_list[idx + 1])
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
+### using our testing script.
+### Because of the small size of the dataset, the script uses stratified
+### shuffle split cross validation. For more info:
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+from sklearn import grid_search
+parameters = {'min_samples_split':[2,4,6,8,10,12,50],
+              'splitter': ('best','random'),
+              'max_depth':[None,2,4,6,8,10,15,20]
+              }
+
+clf_s = grid_search.GridSearchCV(tree.DecisionTreeClassifier(), parameters).fit(features, labels)
+
+test_classifier(clf, new_dataset, features_list)
+
+### Dump your classifier, dataset, and features_list so
+### anyone can run/check your results.
+
+dump_classifier_and_data(clf, new_dataset, features_list)
